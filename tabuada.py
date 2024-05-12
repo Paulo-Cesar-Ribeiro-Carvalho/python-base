f94f9292e6d290c2edbbc1a00f7c45c4abe5594b
Paulo Cesar Ribeiro Carvalho <paulocesar.25jan@gmail.com>
2024-05-12 19:33:43 -0300

    alterada a versão da Tabuada

---
+++ tabuada.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""Imprime a tabuada do 1 ao 10.
+
+---Tabuada do 1---
+
+    1 x 1 = 1
+    2 x 1 = 2
+    3 x 1 = 3
+
+...
+#################
+---Tabuada do 2---
+
+     2 x 1 = 2
+     2 x 2 = 4
+
+...
+#################
+"""
+___version___ = "0.1.1"
+___author___ = "Paulo Cesar"
+
+
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#itarable (percorrivel)
+numeros = list(range(1, 11))
+
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
