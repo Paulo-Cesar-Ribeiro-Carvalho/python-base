0a939f02f4677c0b266c943b96aa90ff0c9529ff
paulo-cesar-ribeiro-carvalho <paulocesar.25jan@gmail.com>
2024-05-28 16:08:42 -0300

    Refaturado em 28mai

--- hello.py
+++ hello.py
@@ -6,9 +6,12 @@ correspondente.
 
 Como usar:
 
-Tenha a variável LANG devidamente configuradaex:
+Tenha a variável LANG devidamente configurada ex:
 
     export LANG=pt_BR
+Ou informe atraves do CLI argument `--lang`
+
+Ou o usuário terá que digitar.
 
 Execução:
 
@@ -16,24 +19,44 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.0.1"
+__version__ = "0.1.3"
 __author__ = "Paulo Cesar"
 __license__ = "Unlicense"
 
+
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello,World!"
+arguments = {"lang": None, "count": 1}
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid option `{key}`")
+        sys.exit()
+    arguments[key] = value
 
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: usar repetição
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else:
+        current_language = input("Chose a language:")
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"
-elif current_language == "es_EP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour, Monde!"
+current_language = current_language[:5]    
+
+msg = {
+    "en_US": "Hello, world!!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_EP": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
+}
     
-print (msg)
+print(
+    msg[current_language] * int(arguments["count"])
+    )
 
