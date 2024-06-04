0894cd57dfb9f97ff74f672c2ffa27c18edb4c71
paulo-cesar-ribeiro-carvalho <paulocesar.25jan@gmail.com>
2024-06-04 10:25:04 -0300

    tratamento de erro

--- hello.py
+++ hello.py
@@ -26,19 +26,43 @@ __license__ = "Unlicense"
 
 import os
 import sys
+import logging
+
+log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
+log = logging.Logger("Paulo", log_level)
+ch = logging.StreamHandler()
+ch.setLevel(log_level)
+fmt = logging.Formatter(
+    '%(asctima)s %(name)s %(levelname)s'
+    'l:%(lineo)d f:%(filename)s: %(message)s'
+)
+ch.setFormatter(fmt)
+log.addHandler(ch)
+
 
 arguments = {"lang": None, "count": 1}
+
 for arg in sys.argv[1:]:
-    # TODO: Tratar ValueError
-    key, value = arg.split("=")
+    try:
+        key, value = arg.split("=")
+    except ValueError as e:
+        log.error (
+            "You need to use '=', you passed %s, try --key=value: %s",
+            arg.
+            str(e)
+        )
+        sys.exit(1)
     key = key.lstrip("-").strip()
     value = value.strip()
+
+# Validação
     if key not in arguments:
         print(f"Invalid option `{key}`")
         sys.exit()
     arguments[key] = value
 
 current_language = arguments["lang"]
+
 if current_language is None:
     # TODO: usar repetição
     if "LANG" in os.environ:
@@ -55,8 +79,20 @@ msg = {
     "es_EP": "Hola, Mundo!",
     "fr_FR": "Bonjour, Monde!",
 }
-    
+
+"""
+# try com valor default
+message = msg.get(current_language, msg["en_US"])
+"""
+#EAFP
+try:
+    message = msg[current_language]
+except KeyError as e:
+    print(f"[ERROR] {str(e)}")
+    print(f"Language is invalid, choose from: {list(msg.keys())}")
+    sys.exit(1)
+
 print(
-    msg[current_language] * int(arguments["count"])
+    message * int(arguments["count"])
     )
 
